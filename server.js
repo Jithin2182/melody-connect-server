12da664b29d28a0783eac048eaf0287f6f514edc
jithin2182 <jithingunnu293@gmail.com>
2025-03-21 00:44:02 +0530

    cors error update

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+app.use(cors({
+  origin: 'https://melody-connect.netlify.app', // Your frontend URL
+}));
+
 // Connect to MongoDB
 mongoose
   .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
